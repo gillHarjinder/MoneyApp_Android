8a11ca9561733b0b3574bb58b680bb9ae64122f1
harjindergill <laddivaliant@live.com>
2017-06-28 12:48:54 -0700

    Remove Category Button and Screen

--- app/src/main/java/com/example/gill/moneyapp_android/MainActivity.java
+++ app/src/main/java/com/example/gill/moneyapp_android/MainActivity.java
@@ -13,14 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Create a button which open the Category_Activity from main screen
-        Button CategoryBtn = (Button)findViewById(R.id.Category_Button);
-        CategoryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Category_Activity.class));
-            }
-        });
 
 
         //Create a button which open the Category_Activity from main screen
