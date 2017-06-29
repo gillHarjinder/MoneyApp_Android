fbb9947ea09bb9949fee603a536849ec14c65147
harjindergill <laddivaliant@live.com>
2017-06-29 11:05:05 -0700

    rebuild the add screen

--- app/src/main/java/com/example/gill/moneyapp_android/add_Activity.java
+++ app/src/main/java/com/example/gill/moneyapp_android/add_Activity.java
@@ -1,13 +1,22 @@
 package com.example.gill.moneyapp_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class add_Activity extends AppCompatActivity {
+public class add_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_);
+
+
     }
+
 }
